5c1a56e758fc85dfb0c3f5a9abcf5b15d6d97332
Davit-2100 <momjyandavit@gmail.com>
2024-12-10 09:52:52 -0800

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,10 +1,11 @@
+//Signed off by QA, tested on cpp.sh, VSCode, Clion on C++ 17 for max compatibility
 #include <iostream>
 using namespace std;
 
 int main() {
-    const int s = 5;
+    const int s = 11;
     int a, b;
-    int arr[] = {5, 3, 8, 4, 6};
+    int arr[] = {7, 4, 8, 1, 5, 9, 2, 6, 10, 3, 12};
     bool swap;
     for (a = 0; a < s; a++) {
         swap = false; // Fixed assignment
